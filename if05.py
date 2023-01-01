d7551c299d9d46e6d66c24072f16fe94112dc608
shoxrux0607 <chullievshoxrux0607@gmail.com>
2023-01-01 18:04:55 +0500

    05

--- if05.py
+++ if05.py
@@ -6,4 +6,31 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    x1 = n % 10
+    n = n//10
+
+    x2 = n % 10
+    n = n//10
+
+    x3 = n % 10
+    n = n//10
+
+    x4 = n % 10
+    n = n//10
+
+    x5 = n % 10
+    n = n//10
+
+    if x1 >= x2 and x1 >= x3 and x1 >= x4 and x1 >= x5:
+        return x1
+    elif x2 >= x1 and x2 >= x3 and x2 >= x4 and x2 >= x5:
+        return x2
+    elif x3 >= x1 and x3 >= x2 and x3 >= x4 and x3 >= x5:
+        return x3
+    elif x4 >= x1 and x4 >= x2 and x4 >= x3 and x4 >= x5:
+        return x4
+    else:
+        return x5
+
+
+print(main(50395))
