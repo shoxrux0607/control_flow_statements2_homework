637098a9e94b4be9abd6a146d65718fe612fe1f0
shoxrux0607 <chullievshoxrux0607@gmail.com>
2023-01-01 17:36:17 +0500

    03

--- if03.py
+++ if03.py
@@ -8,4 +8,20 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
+    between = c
+    if a<=b:
+        if b<=c:
+            between=b
+        elif a>=c:
+            between=a
+    else:
+        if a<=c:
+            between=a
+        elif b>=c:
+            between=b
+
+
+    return between  
+
+
+print(main(2000,3100,440))
