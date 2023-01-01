47b7eb508fbaf47f7d06bf6a6d65879439dc6106
shoxrux0607 <chullievshoxrux0607@gmail.com>
2023-01-01 18:24:54 +0500

    08

--- if08.py
+++ if08.py
@@ -14,4 +14,19 @@ def main(number):
     Returns:
         str: return answer.
     """
-    return
+    if number == 1:
+        return "Monday"
+    elif number == 2:
+        return "Tuesday"
+    elif number == 3:
+        return "Wednesday"
+    elif number == 4:
+        return "Thursday"
+    elif number == 5:
+        return "Friday"
+    elif number == 6:
+        return "Saturday"
+    elif number == 7:
+        return "Sunday"
+
+print(main(9))
