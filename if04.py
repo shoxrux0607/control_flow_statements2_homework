cdcae47e26c91f2ced4985a8a6829bf6a261df37
shoxrux0607 <chullievshoxrux0607@gmail.com>
2023-01-01 17:41:15 +0500

    04

--- if04.py
+++ if04.py
@@ -1,10 +1,17 @@
 def main(a,b):
     """
-    Return zero if the numbers are equal, return the larger one if they are not equal.
+    Return zero if the numbers are equal, 
+    return the larger one if they are not equal.
     Args:
         a: First number.
         b: Second number.
     Returns:
         int: return answer.
     """
-    return
+    if a==b:
+        return 0
+    elif a>b:
+        return a
+    else:
+        return b
+print(main(13,13))
